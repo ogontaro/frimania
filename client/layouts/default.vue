b12ccc05ad94eb22c6dd52f92fde963e5e692be1
ogontaro <ogontaro@gmail.com>
2018-08-29 13:50:38 +0900

    add sample navbar

--- client/layouts/default.vue
+++ client/layouts/default.vue
@@ -1,7 +1,16 @@
 <template>
-  <div>
-    <nuxt/>
-  </div>
+    <div>
+        <navbar/>
+        <nuxt/>
+    </div>
 </template>
 
+<script>
+  import Navbar from '~/components/Navbar.vue'
 
+  export default {
+    components: {
+      Navbar
+    }
+  }
+</script>
