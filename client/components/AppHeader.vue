7e3bb9d6489201ae78416236566db7686f6a29a3
ogontaro <ogontaro@gmail.com>
2018-10-04 11:39:59 +0900

    add logo

--- client/components/AppHeader.vue
+++ client/components/AppHeader.vue
@@ -2,5 +2,6 @@
   header
     nav.navbar(role='navigation' area-label='main navigation')
       .navbar-brand
-        a(href='/') frimania
+        a.navbar-item(href='/')
+          img(src="~/static/logo.png")
 </template>
