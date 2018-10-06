163f48e433c3a93cb817428cbc7c0b130f32c079
ogontaro <ogontaro@gmail.com>
2018-10-06 15:14:40 +0900

    add navigation inline form in AppHeader

--- client/components/AppHeader.vue
+++ client/components/AppHeader.vue
@@ -4,4 +4,16 @@
       .navbar-brand
         a.navbar-item(href='/')
           img(src="~/static/logo.png")
+        form.field.navbar-item.is-expanded.p-is-expanded.has-addons
+          .control.has-icons-right.p-is-expanded
+            input.input(type="text" placeholder="検索キーワードを入力")
+            span.icon.is-right
+              i.fas.fa-search
+          .control
+            a.button.is-info 検索
 </template>
+<style scoped lang="scss">
+  .p-is-expanded {
+    width: 100%;
+  }
+</style>
